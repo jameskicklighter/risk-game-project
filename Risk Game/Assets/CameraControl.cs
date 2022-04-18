4fa12d1e82f6a097ccfc1472705e37b448662c60
jameskicklighter <jameskick@hotmail.com>
2022-04-18 11:31:17 -0400

    Added Zoom Feature
    Zooming is now enabled via the Camera Control script.

--- Risk Game/Assets/CameraControl.cs
+++ Risk Game/Assets/CameraControl.cs
@@ -6,6 +6,7 @@ public class CameraControl : MonoBehaviour {
 	// Start is called before the first frame update
 	private float speed = 0.5f;
 	private float verticalBound = 4.09f;
+	private float zoomSize;
 
 	void Start() {
 
@@ -13,21 +14,58 @@ public class CameraControl : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
+		zoomSize = gameObject.GetComponent<Camera>().orthographicSize;
+
 		if (transform.position.y > verticalBound) {
 			transform.position = new Vector2(transform.position.x, verticalBound);
 		}
+
 		if (transform.position.y < -verticalBound) {
 			transform.position = new Vector2(transform.position.x, -verticalBound);
 		}
-		if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
-			MoveCameraUp();
-			Debug.Log("up");
+
+		if (Input.GetKey(KeyCode.LeftControl) == false) {
+			if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+				MoveCameraUp();
+				Debug.Log("up");
+			}
+			else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+				MoveCameraDown();
+				Debug.Log("down");
+			}
 		}
-		if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
-			MoveCameraDown();
-			Debug.Log("down");
+		else if (zoomSize >= 7) {
+			if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+				ZoomIn();
+				Debug.Log("zoom in");
+			}
 		}
-		//gameObject.GetComponent<Camera>().orthographicSize = 7;
+		else if (zoomSize <= 3.5) {
+			if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+				ZoomOut();
+				Debug.Log("zoom out");
+			}
+		}
+		else {
+			if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+				ZoomIn();
+				Debug.Log("zoom in");
+			}
+			else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+				ZoomOut();
+				Debug.Log("zoom out");
+			}
+		}
+	}
+
+	void ZoomIn() {
+		zoomSize -= 0.5f;
+		gameObject.GetComponent<Camera>().orthographicSize = zoomSize;
+	}
+
+	void ZoomOut() {
+		zoomSize += 0.5f;
+		gameObject.GetComponent<Camera>().orthographicSize = zoomSize;
 	}
 
 	void MoveCameraUp() {
